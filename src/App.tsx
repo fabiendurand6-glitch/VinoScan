66f628c1a6a149967e04910f651669bad68548a9
fabiendurand6-glitch <fabiendurand6@gmail.com>
2026-05-09 15:08:41 +0200

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import {
 } from 'firebase/firestore';
 
 // --- API & FIREBASE CONFIGURATION ---
-const apiKey = "AIzaSyClDDsccuRWaxBMQpMUNgII81AxlOxYclc"; // <-- COLlez VOTRE CLÉ GEMINI ICI (ex: "AIzaSy...")
+const apiKey = "AIzaSyBom4kXpkpVQSMLS5k8RYKgh8PDLqfwEm0"; // <-- COLlez VOTRE CLÉ GEMINI ICI (ex: "AIzaSy...")
 
 // VOTRE CONFIGURATION FIREBASE
 const firebaseConfig = { 
