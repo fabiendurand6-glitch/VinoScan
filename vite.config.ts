cb82dfdcb0380018f5dd60b2b1d56e71261f813c
null <null>
2026-05-26 18:39:36 +0200

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Ajoute cette section pour forcer la compilation correcte
+  optimizeDeps: {
+    include: ['recharts', 'es-toolkit']
+  },
   build: {
-    minify: false // Ajoute cette ligne pour le débogage
+    // Tu peux retirer le minify: false maintenant qu'on a trouvé le problème !
   }
 })
